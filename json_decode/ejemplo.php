485abc3ac23b45d673c7933e2fa5c1094f5f91e0
asynkDF <mrlgsus22@gmail.com>
2023-06-23 16:15:53 -0500

    feat: :ambulance: 8, 9 y 10

--- json_decode/ejemplo.php
+++ json_decode/ejemplo.php
@@ -1,37 +1,50 @@
 <?php
-$datos = '[
-{
-    "nombre": "Goku",
-    "edad:" 32,
-    "telefono": 3166666
-},
-{
-    "nombre": "Saitama",
-    "edad:" 2,
-    "telefono": 3123456
-}
-]';
-var_dump($datos);
-$nuevosDatos = json_decode($datos, true);
-var_dump($nuevosDatos);
-foreach ($nuevosDatos[0] as $key => $value){ //en este casi se muestran los de Goku por la posicion 0
-    echo $key;
-}
-//asi se veria:
-$nuevosDatos = array(
-    0 => array{
-        "nombre": "Goku",
-        "edad": 32,
-        "telefono": 3166666
 
+$datosEmpleadosJSON = '
+[
+    {
+        "nombre": "Camilo",
+        "edad": 30,
+        "ciudad": "Caracas"
     },
-    1 => array{
-        "nombre": "Saitama",
-        "edad": 2,
-        "telefono": 3123456
-
+    {
+        "nombre": "Leandro",
+        "edad": 26,
+        "ciudad": "Cali"
     },
-)
+    {
+        "nombre": "Pepito",
+        "edad": 79,
+        "ciudad": "Bucaramanga"
+    }
+]
+';
 
+var_dump($datosEmpleadosJSON);
+
+echo "<br>";
+
+$datosNuevos = json_decode($datosEmpleadosJSON, true);
+
+echo "<br>";
+
+var_dump($datosNuevos);
+
+echo "<br><br>";
+
+
+foreach ($datosNuevos[0] as $key => $value) {
+    echo $value;
+    echo "<br>";
+    
+}
+
+echo "<br><br>";
+
+foreach ($datosNuevos as $key => $value) {
+    echo $value['nombre'] . " " . $value['edad'] . " " . $value['ciudad'];
+    echo "<br>";
+    
+}
 
 ?>
